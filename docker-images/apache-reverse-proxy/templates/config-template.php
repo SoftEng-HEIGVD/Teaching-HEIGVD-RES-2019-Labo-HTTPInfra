87e4475e95348baac7cbc2554662dbdeae09630d
Ichimizu <arzur.cateltorres@heig-vd.ch>
2019-06-09 09:59:37 +0200

    Add balancing functionality

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,13 +1,32 @@
-<?php 
-	$dynamic_app = getenv('DYNAMIC_APP');
-	$static_app = getenv('STATIC_APP');
+<?php
+  $dynamic_app1 = getenv('DYNAMIC_APP1');
+  $dynamic_app2 = getenv('DYNAMIC_APP2');
+  $static_app1 = getenv('STATIC_APP1');
+  $static_app2 = getenv('STATIC_APP2');
 ?>
+
 <VirtualHost *:80>
-	ServerName demo.res.ch
+  ServerName demo.res.ch
 
-	ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-	ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+  <Proxy "balancer://dynamic_app">
+    BalancerMember 'http://<?php print "$dynamic_app1"?>'
+    BalancerMember 'http://<?php print "$dynamic_app2"?>'
+  </Proxy>
+  <Proxy "balancer://static_app">
+    BalancerMember 'http://<?php print "$static_app1"?>/'
+    BalancerMember 'http://<?php print "$static_app2"?>/'
+  </Proxy>
 
-	ProxyPass '/' 'http://<?php print "$static_app"?>/'
-    ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+  ProxyPass '/api/students/' 'balancer://dynamic_app/'
+  ProxyPassReverse '/api/students/' 'balancer://dynamic_app/'
+
+  ProxyPass '/' 'balancer://static_app/'
+  ProxyPassReverse '/' 'balancer://static_app/'
+
+  # Ajouter dans httpd.conf
+  # <Location "/balancer-manager">
+  #   SetHandler balancer-manager
+  #   Require host demo.res.ch
+  # </Location>
+
 </VirtualHost>
--- docker-images/start.sh
+++ docker-images/start.sh
@@ -19,20 +19,26 @@ docker build -t res/apache_rp ./apache-reverse-proxy/
 echo " "
 
 echo "########### Run apache_static container ###########"
-docker run -d --name apache_static res/apache_static
+docker run -d res/apache_php #useless
+docker run -d res/apache_php #useless
+docker run -d --name apache_static1 res/apache_php
+docker run -d --name apache_static2 res/apache_php
 echo " "
 
-echo "########### Run express_dynamic ###########"
-docker run -d --name express_students res/express_students
+echo "########### Run express_students ###########"
+docker run -d res/express_students #useless
+docker run -d res/express_students #useless
+docker run -d --name express_dynamic1 res/express_students
+docker run -d --name express_dynamic2 res/express_students
+
 echo " "
 
 echo "########### Run apache_rp ###########"
-docker run -d -p 8080:80 -e STATIC_APP=172.17.0.2:80 -e DYNAMIC_APP=172.17.0.3:3000 --name apache_rp res/apache_rp
+static_app1=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' apache_static1`
+static_app2=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' apache_static2`
+dynamic_app1=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' express_dynamic1`
+dynamic_app2=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' express_dynamic2`
 echo " "
 
-echo "########### check ip apache_static container (should be 172.17.0.2) ###########"
-docker inspect apache_static | grep -i ipaddress
-echo " "
-
-echo "########### check ip express_dynamic conatiner (should be 172.17.0.3) ###########"
-docker inspect express_students | grep -i ipaddress
+echo "## IP of injected: static $static_app1, $static_app2 and dynamic $dynamic_app1, $dynamic_app2\n" 
+docker run -d -p 8080:80 -e STATIC_APP1=$static_app1:80 -e STATIC_APP2=$static_app2:80 -e DYNAMIC_APP1=$dynamic_app1:3000 -e DYNAMIC_APP2=$dynamic_app2:3000 --name apache_rp res/apache_rp
